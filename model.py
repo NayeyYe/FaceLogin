274eb65384dba3a845d6c1a275b407a275639c48
NayeyYe <Nayey.Ye@outlook.com>
2025-05-04 18:43:47 +0800

    update model

--- model.py
+++ model.py
@@ -39,7 +39,42 @@ class PNet(nn.Module):
         landmark_out = torch.squeeze(landmark_out, dim=2)
         return class_out, bbox_out, landmark_out
 
+class RNet(nn.Module):
+    def __init__(self):
+        super(RNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=28, kernel_size=(3, 3))
+        self.prelu1 = nn.PReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
+        self.conv2 = nn.Conv2d(in_channels=28, out_channels=48, kernel_size=(3, 3))
+        self.prelu2 = nn.PReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
+        self.conv3 = nn.Conv2d(in_channels=48, out_channels=64, kernel_size=(2, 2))
+        self.prelu3 = nn.PReLU()
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(in_features=576, out_features=128)
+        self.class_fc = nn.Linear(in_features=128, out_features=2)
+        self.bbox_fc = nn.Linear(in_features=128, out_features=4)
+        self.landmark_fc = nn.Linear(in_features=128, out_features=10)
 
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x):
+        x = self.prelu1(self.conv1(x))
+        x = self.pool1(x)
+        x = self.prelu2(self.conv2(x))
+        x = self.pool2(x)
+        x = self.prelu3(self.conv3(x))
+        x = self.flatten(x)
+        x = self.fc(x)
+        # 分类是否人脸的卷积输出层
+        class_out = self.class_fc(x)
+        # 人脸box的回归卷积输出层
+        bbox_out = self.bbox_fc(x)
+        # 5个关键点的回归卷积输出层
+        landmark_out = self.landmark_fc(x)
+        return class_out, bbox_out, landmark_out
 
 class ONet(nn.Module):
     def __init__(self):
@@ -83,79 +118,7 @@ class ONet(nn.Module):
         landmark_out = self.landmark_fc(x)
         return class_out, bbox_out, landmark_out
 
-class PNet(nn.Module):
-    def __init__(self):
-        super(PNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=(3, 3))
-        self.prelu1 = nn.PReLU()
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)
-        self.conv2 = nn.Conv2d(in_channels=10, out_channels=16, kernel_size=(3, 3))
-        self.prelu2 = nn.PReLU()
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3))
-        self.prelu3 = nn.PReLU()
-        self.conv4_1 = nn.Conv2d(in_channels=32, out_channels=2, kernel_size=(1, 1))
-        self.conv4_2 = nn.Conv2d(in_channels=32, out_channels=4, kernel_size=(1, 1))
-        self.conv4_3 = nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1))
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal(m.weight, mode='fan_out', nonlinearity='relu')
-
-    def forward(self, x):
-        x = self.prelu1(self.conv1(x))
-        x = self.pool1(x)
-        x = self.prelu2(self.conv2(x))
-        x = self.prelu3(self.conv3(x))
-        # 分类是否人脸的卷积输出层
-        class_out = self.conv4_1(x)
-        class_out = torch.squeeze(class_out, dim=2)
-        class_out = torch.squeeze(class_out, dim=2)
-        # 人脸box的回归卷积输出层
-        bbox_out = self.conv4_2(x)
-        bbox_out = torch.squeeze(bbox_out, dim=2)
-        bbox_out = torch.squeeze(bbox_out, dim=2)
-        # 5个关键点的回归卷积输出层
-        landmark_out = self.conv4_3(x)
-        landmark_out = torch.squeeze(landmark_out, dim=2)
-        landmark_out = torch.squeeze(landmark_out, dim=2)
-        return class_out, bbox_out, landmark_out
-
-class RNet(nn.Module):
-    def __init__(self):
-        super(RNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=28, kernel_size=(3, 3))
-        self.prelu1 = nn.PReLU()
-        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
-        self.conv2 = nn.Conv2d(in_channels=28, out_channels=48, kernel_size=(3, 3))
-        self.prelu2 = nn.PReLU()
-        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
-        self.conv3 = nn.Conv2d(in_channels=48, out_channels=64, kernel_size=(2, 2))
-        self.prelu3 = nn.PReLU()
-        self.flatten = nn.Flatten()
-        self.fc = nn.Linear(in_features=576, out_features=128)
-        self.class_fc = nn.Linear(in_features=128, out_features=2)
-        self.bbox_fc = nn.Linear(in_features=128, out_features=4)
-        self.landmark_fc = nn.Linear(in_features=128, out_features=10)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal(m.weight, mode='fan_out', nonlinearity='relu')
-
-    def forward(self, x):
-        x = self.prelu1(self.conv1(x))
-        x = self.pool1(x)
-        x = self.prelu2(self.conv2(x))
-        x = self.pool2(x)
-        x = self.prelu3(self.conv3(x))
-        x = self.flatten(x)
-        x = self.fc(x)
-        # 分类是否人脸的卷积输出层
-        class_out = self.class_fc(x)
-        # 人脸box的回归卷积输出层
-        bbox_out = self.bbox_fc(x)
-        # 5个关键点的回归卷积输出层
-        landmark_out = self.landmark_fc(x)
-        return class_out, bbox_out, landmark_out
 
 
 class ClassLoss(nn.Module):
